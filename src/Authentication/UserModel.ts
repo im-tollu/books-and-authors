52b80015764f7c7cc4b0cd3aa227c6acb56bdeb0
Ilja Tollu <im.tollu@gmail.com>
2022-12-30 01:27:16 +0200

    fix typo

--- src/Authentication/UserModel.ts
+++ src/Authentication/UserModel.ts
@@ -4,16 +4,16 @@ import { computed, makeObservable, observable } from 'mobx'
 @injectable()
 export class UserModel {
     email: string | null = null
-    token: string | null = null =
+    token: string | null = null
 
-        constructor() {
-    makeObservable(this, {
-        token: observable,
-        isLoggedIn: computed,
-    })
-}
+    constructor() {
+        makeObservable(this, {
+            token: observable,
+            isLoggedIn: computed,
+        })
+    }
 
     get isLoggedIn() {
-    return this.token !== null && this.email !== null
-}
+        return this.token !== null && this.email !== null
+    }
 }
