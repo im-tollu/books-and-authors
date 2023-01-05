ced43681b65cf8db659d54736ba406c1f6bff8bb
Ilia Tolliu <im.tollu@gmail.com>
2023-01-05 17:56:54 +0000

    make useEffect depend on presenter

--- src/Authors/AuthorsComponent.tsx
+++ src/Authors/AuthorsComponent.tsx
@@ -1,34 +1,38 @@
-import { useInjection } from '../Core/Providers/Injection';
-import { AuthorsPresenter } from './AuthorsPresenter';
-import { AuthorListComponent } from './AuthorListComponent';
-import { AddAuthorComponent } from './AddAuthorComponent';
-import { AddBookComponent } from '../Books/AddBookComponent';
-import { BookListComponent } from '../Books/BookListComponent';
-import { MessagesComponent } from '../Core/Messages/MessagesComponent';
-import { namedObserver } from '../Core/Observer';
-import { FC, useEffect } from 'react';
+import { useInjection } from "../Core/Providers/Injection";
+import { AuthorsPresenter } from "./AuthorsPresenter";
+import { AuthorListComponent } from "./AuthorListComponent";
+import { AddAuthorComponent } from "./AddAuthorComponent";
+import { AddBookComponent } from "../Books/AddBookComponent";
+import { BookListComponent } from "../Books/BookListComponent";
+import { MessagesComponent } from "../Core/Messages/MessagesComponent";
+import { namedObserver } from "../Core/Observer";
+import { FC, useEffect } from "react";
 
-export const AuthorsComponent: FC = namedObserver('AuthorsComponent', () => {
-    const presenter = useInjection(AuthorsPresenter)
+export const AuthorsComponent: FC = namedObserver("AuthorsComponent", () => {
+  const presenter = useInjection(AuthorsPresenter);
 
-    useEffect(() => {
-        presenter.load()
-    }, [])
+  useEffect(() => {
+    presenter.load();
+  }, [presenter]);
 
-    return (
-        <>
-            <h1>Authors</h1>
-            <input type="button" value="show author list" onClick={presenter.toggleShowAuthorsList} />
-            <br />
-            <AuthorListComponent />
-            <br />
-            <AddAuthorComponent />
-            <br />
-            <AddBookComponent presenter={presenter} />
-            <br />
-            <BookListComponent />
-            <br />
-            <MessagesComponent />
-        </>
-    )
-})
+  return (
+    <>
+      <h1>Authors</h1>
+      <input
+        type="button"
+        value="show author list"
+        onClick={presenter.toggleShowAuthorsList}
+      />
+      <br />
+      <AuthorListComponent />
+      <br />
+      <AddAuthorComponent />
+      <br />
+      <AddBookComponent presenter={presenter} />
+      <br />
+      <BookListComponent />
+      <br />
+      <MessagesComponent />
+    </>
+  );
+});
